79b35ad6536bdb70060d2efc2cd63ba6c94d45c3
kinda-neat <feedback.maksimus@gmail.com>
2020-05-03 12:47:13 +0700

    send echo messages in reply to user's message

--- src/TelegramAPI.hs
+++ src/TelegramAPI.hs
@@ -36,27 +36,49 @@ pollServer config offset =
            then "offset" =: fromJust offset
            else mempty)
     let updates = parseEither parseGetUpdatesResponse (responseBody s)
-    liftIO $
-      case updates of
-        Left e -> putStrLn $ show e
-        Right _updates -> do
-          let newLastUpdateId = getNewLastUpdateId (result _updates) offset
-          putStrLn $ "Polling server..."
-          if newLastUpdateId == offset
-            then pollServer config offset
-            else do
-              let lastMessage = message . last $ result _updates
-              let messageFrom = from lastMessage
+    case updates of
+      Left e -> liftIO $ putStrLn $ show e
+      Right _updates -> do
+        let newLastUpdateId = getNewLastUpdateId (result _updates) offset
+        liftIO $ putStrLn $ "Polling server..."
+        if newLastUpdateId == offset
+          then liftIO $ pollServer config offset
+          else do
+            let lastMessage = message . last $ result _updates
+                messageFrom = from lastMessage
+                lastMessageChatId = chatId . mChat $ lastMessage
+            mapM_
+              (\x ->
+                 runReq
+                   (defaultHttpConfig {httpConfigProxy = makeProxyConfig config})
+                   x)
+              (replicate 5 $
+               (sendMessageRequest
+                  config
+                  lastMessageChatId
+                  ("Got message from you: " ++ text lastMessage)))
+            liftIO $
               putStrLn $
-                show $
-                "New message: " ++
-                text lastMessage ++
-                " from username = " ++
-                username messageFrom ++
-                " and name = " ++
-                first_name messageFrom ++ " " ++ last_name messageFrom
-              putStrLn $ "Made a new request"
-              pollServer config newLastUpdateId
+              show $
+              "New message: " ++
+              text lastMessage ++
+              " from username = " ++
+              username messageFrom ++
+              " and name = " ++
+              first_name messageFrom ++ " " ++ last_name messageFrom
+            liftIO $ putStrLn $ "Made a new request"
+            liftIO $ pollServer config newLastUpdateId
+
+sendMessageRequest ::
+     TelegramConfig -> Integer -> String -> Req (JsonResponse Value)
+sendMessageRequest config chatId text =
+  req
+    GET
+    (https "api.telegram.org" /: (T.pack $ "bot" ++ token config) /:
+     "sendMessage")
+    NoReqBody
+    jsonResponse
+    ("chat_id" =: chatId <> "text" =: text)
 
 parseGetUpdatesResponse :: Value -> Parser (TResponse [TUpdate])
 parseGetUpdatesResponse = parseJSON
@@ -88,11 +110,13 @@ data TMessage = TMessage
   { message_id :: Integer
   , text :: String
   , from :: TMessageFrom
+  , mChat :: TChat
   } deriving (Show)
 
 instance FromJSON TMessage where
   parseJSON (Object o) =
-    TMessage <$> o .: "message_id" <*> o .: "text" <*> o .: "from"
+    TMessage <$> o .: "message_id" <*> o .: "text" <*> o .: "from" <*>
+    o .: "chat"
 
 data TMessageFrom = TMessageFrom
   { id :: Integer
@@ -107,3 +131,10 @@ instance FromJSON TMessageFrom where
     TMessageFrom <$> o .: "id" <*> o .: "first_name" <*> o .: "last_name" <*>
     o .: "username" <*>
     o .: "language_code"
+
+data TChat = TChat
+  { chatId :: Integer
+  } deriving (Show)
+
+instance FromJSON TChat where
+  parseJSON (Object o) = TChat <$> o .: "id"
